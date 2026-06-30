670786c64426558eccdb5a766bf3a5bbdba087e2
Alex <177274285+alextecnicoprogramador@users.noreply.github.com>
2026-06-29 21:46:03 -0300

    projeto completo

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int numero;
+        String agencia;
+        String nomeCliente;
+        BigDecimal saldo;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o número da conta: ");
+        numero = teclado.nextInt();
+        teclado.nextLine();
+        System.out.println("Por favor, digite o número da agencia: ");
+        agencia = teclado.nextLine();
+        System.out.println("Digite seu nome: ");
+        nomeCliente = teclado.nextLine();
+        System.out.println("Digite o valor que irá depositar: ");
+        saldo = teclado.nextBigDecimal();
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco," +
+                "sua agência é %s, conta %s e seu saldo R$%.2f " +
+                "já está disponível para saque", nomeCliente, agencia, numero, saldo);
+        teclado.close();
     }
 }
